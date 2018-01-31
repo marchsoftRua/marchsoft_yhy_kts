b1148278bae81e15c4fb818603fa11896d17b23a
sss2016 <1079783140@qq.com>
2018-01-31 10:19:59 +0800

    alter model

---
+++ resources/views/alert.blade.php
@@ -0,0 +1,5 @@
+<div class="alert alert-danger">
+    {{$title}}
+    {{$content}}
+    {{--{{ $slot }}--}}
+</div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,12 +79,21 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+
+                    @component('alert')
+                        @slot('title')
+                            Whoops
+                        @endslot
+                        @slot('content')
+                            <strong>!555</strong>
+                        @endslot
+
+                    @endcomponent
                 </div>
 
                 <div class="links">
                     <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts三月软件666</a>
+                    <a href="https://laracasts.com">Laracasts</a>
                     <a href="https://laravel-news.com">News</a>
                     <a href="https://forge.laravel.com">Forge</a>
                     <a href="https://github.com/laravel/laravel">GitHub</a>
